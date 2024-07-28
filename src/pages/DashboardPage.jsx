0ebf09b276b364a0e9eb1f0ba54558cc863e2b43
automationblog <sawin24@gmail.com>
2024-07-28 19:59:52 +0530

    dashboard updated

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
-import { Navbar, Nav, Dropdown, Image, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Dropdown,
+  Image,
+  Button,
+  Container,
+} from "react-bootstrap";
 import { UserDetailsApi } from "../services/Api";
 import { isAuthenticated } from "../services/Auth";
 
@@ -21,32 +28,58 @@ const DashboardPage = () => {
     return <Navigate to="/" />;
   };
 
+  const [showDropdown, setShowDropdown] = useState(false);
+  const handleDropdownToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   if (!isAuthenticated()) {
     return <Navigate to="/Login" />;
   }
 
   return (
-    <main role="main" className="container mt-5">
-      <div className="container">
+    <>
+      <Navbar bg="light" expand="sm" className="flex-row">
+        <Navbar.Brand href="/dashboard">
+          <span style={{ fontWeight: "2rem", marginLeft: "10px" }}>Home</span>
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Dropdown show={showDropdown} onToggle={handleDropdownToggle}>
+              <Dropdown.Toggle variant="button" id="dropdown-basic">
+                <Image
+                  src="/profile-picture.png"
+                  roundedCircle
+                  style={{ width: "30px", height: "30px" }}
+                />
+                <span className="ml-2">{userData.name.toUpperCase()}</span>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item>My Profile</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>
+                  <Nav.Link
+                    onClick={handleLogout}
+                    style={{ cursor: "pointer" }}
+                    to="/"
+                    as={Link}
+                  >
+                    Logout
+                  </Nav.Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container className="mt-5" fluid>
         <div className="text-center mt-5">
           <h3>Dashboard page</h3>
-
-          {userData.name && userData.email ? (
-            <div>
-              <p className="text-bold ">Hi {userData.name},</p>
-              <p className="text-bold ">Your Email is {userData.email}</p>
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
+          <p className="text-bold ">Hi {userData.name},</p>
+          <p className="text-bold ">Your Email is {userData.email}</p>
         </div>
-        <Link to="/">
-          <Button onClick={handleLogout} variant="primary" className="mt-3">
-            Logout
-          </Button>
-        </Link>
-      </div>
-    </main>
+      </Container>
+    </>
   );
 };
 export default DashboardPage;
